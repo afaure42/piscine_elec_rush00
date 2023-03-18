14fbeb4763b07c8f480fb1ac9b57467f1aef7896
dhubleur <dhubleur@student.42.fr>
2023-03-18 15:43:32 +0100

    count time

--- ex00/slave.c
+++ ex00/slave.c
@@ -5,14 +5,28 @@
 
 #include "uart.h"
 
+void stop_game_waiting();
+
 volatile int32_t timer_count = 0;
 
-ISR(TIMER1_OVF_vect) {
+//Timer 1 counted 4 secondes
+ISR(TIMER1_COMPA_vect) {
 	timer_count++;
-	uart_putnbr(timer_count);
 }
 
-void start_game_timer(void) {
+//Button SW1 pressed
+ISR(INT0_vect) {
+	//Get timer value
+	int32_t timer_ms = TCNT1 / 15.625;
+	//Add stored timer value
+	int32_t total_ms = timer_count * 4000 + timer_ms;
+	uart_printstr("Elapsed time: ");
+	uart_putnbr(total_ms);
+	uart_printstr("ms\r\n");
+	stop_game_waiting();
+}
+
+void start_game_waiting(void) {
 	//Start timer 1
 	//Set prescaler to 1024 (16000000/1024 = 15625Hz) 
 	//Set timer to CTC mode
@@ -20,22 +34,35 @@ void start_game_timer(void) {
 	//Set OCR1A to 4 * 15625 = 62500
 	//Enable interrupt on OCR1A
 	TCNT1 = 0;
-
 	TCCR1A = 0;
 	TCCR1B = (1 << WGM12);
-
 	TCCR1B |= (1 << CS12) | (1 << CS10);
-
 	OCR1A = 62500;
-
 	TIMSK1 |= (1 << OCIE1A);
 
-	sei();
+	//Setup button SW1 as input
+	//Detect falling edge on INT0
+	//Enable interrupt on INT0
+	DDRD &= ~(1 << PD2);
+	EICRA |= (1 << ISC01) | (0 << ISC00);
+	EIMSK |= (1 << INT0);
+}
+
+void stop_game_waiting() {
+	//Disable interrupt on INT0
+	EIMSK &= ~(1 << INT0);
+	//Disable interrupt on OCR1A
+	TIMSK1 &= ~(1 << OCIE1A);
+	//Stop timer 1
+	TCCR1B = 0;
+	timer_count = 0;
 }
 
 int main() {
 	uart_init();
-	start_game_timer();
+	start_game_waiting();
+
+	sei();
 
 	for(;;);
 }
