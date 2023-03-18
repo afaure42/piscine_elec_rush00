17360ac23fafdbf766f10a795b4d21585e24ebe4
dhubleur <dhubleur@student.42.fr>
2023-03-18 17:54:40 +0100

    add basic logic for slaves

--- ex00/slave.c
+++ ex00/slave.c
@@ -1,43 +1,97 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <util/twi.h>
-
-#include "uart.h"
-#include "i2c.h"
+#include "slave.h"
 
 void stop_game_waiting();
 
 volatile int32_t timer_count = 0;
 volatile int32_t clicked_ms = 0;
+volatile int button_pressed = 0;
+volatile slave_state current_slave_state = SLAVE_STATE_STARTING;
 
-//Timer 1 counted 4 secondes
+//Timer 1 counted 1 secondes
 ISR(TIMER1_COMPA_vect) {
 	timer_count++;
+	if (timer_count == (TIMER - 1) / 4) {
+		PORTB |= (1 << LED1);
+	}
+	if (timer_count == (TIMER - 1) / 2) {
+		PORTB |= (1 << LED2);
+	}
+	if (timer_count == (TIMER - 1) * 3 / 4) {
+		PORTB |= (1 << LED3);
+	}
+	if (timer_count == (TIMER - 1)) {
+		PORTB |= (1 << LED4);
+	}
+	if (timer_count == TIMER) {
+		PORTB &= ~(1 << LED1);
+		PORTB &= ~(1 << LED2);
+		PORTB &= ~(1 << LED3);
+		PORTB &= ~(1 << LED4);
+	}
 }
 
 //Button SW1 pressed
 ISR(INT0_vect) {
-	//Get timer value
-	int32_t timer_ms = TCNT1 / 15.625;
-	//Add stored timer value
-	clicked_ms = timer_count * 4000 + timer_ms;
-	stop_game_waiting();
+	if (current_slave_state == SLAVE_STATE_STARTING) {
+		button_pressed = 1;
+		PORTD |= (1 << LED_G);
+		PORTD &= ~(1 << LED_R);
+
+		uart_printstr("Set user ready\r\n");
+
+		//DEBUG
+		_delay_ms(1000);
+		command_start();
+		return;
+	} else if (current_slave_state == SLAVE_STATE_PLAYING && clicked_ms == 0) {
+		//Get timer value
+		int32_t timer_ms = TCNT1 / 15.625;
+		//Add stored timer value
+		clicked_ms = timer_count * 1000 + timer_ms;
+		stop_game_waiting();
+		PORTB &= ~(1 << LED1);
+		PORTB &= ~(1 << LED2);
+		PORTB &= ~(1 << LED3);
+		PORTB &= ~(1 << LED4);
+
+		uart_printstr("Button pressed after ");
+		uart_putnbr(clicked_ms);
+		uart_printstr("ms\r\n");
+
+		//DEBUG
+		_delay_ms(1000);
+		command_win();
+		_delay_ms(2000);
+		command_reset();
+	}
 }
 
 void start_game_waiting(void) {
 	//Start timer 1
 	//Set prescaler to 1024 (16000000/1024 = 15625Hz) 
 	//Set timer to CTC mode
-	//Max value of 16bit timer is 65535 (65535/15625 = ~4.2 => use 4s to be precise)
-	//Set OCR1A to 4 * 15625 = 62500
+	//Max value of 16bit timer is 65535
+	//Set OCR1A to 1s 
 	//Enable interrupt on OCR1A
 	TCNT1 = 0;
 	TCCR1A = 0;
 	TCCR1B = (1 << WGM12);
 	TCCR1B |= (1 << CS12) | (1 << CS10);
-	OCR1A = 62500;
+	OCR1A = 15625;
 	TIMSK1 |= (1 << OCIE1A);
+}
+
+void stop_game_waiting() {
+	//Stop timer 1
+	TCCR1B = 0;
+	timer_count = 0;
+}
+
+int main() {
+	DDRD |= (1 << LED_R) | (1 << LED_G) | (1 << LED_B);
+	DDRB |= (1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4);
+	uart_init();
+	i2c_init_as_slave();
 
 	//Setup button SW1 as input
 	//Detect falling edge on INT0
@@ -45,28 +99,10 @@ void start_game_waiting(void) {
 	DDRD &= ~(1 << PD2);
 	EICRA |= (1 << ISC01) | (0 << ISC00);
 	EIMSK |= (1 << INT0);
-}
-
-void stop_game_waiting() {
-	//Disable interrupt on INT0
-	EIMSK &= ~(1 << INT0);
-	//Disable interrupt on OCR1A
-	TIMSK1 &= ~(1 << OCIE1A);
-	//Stop timer 1
-	TCCR1B = 0;
-	timer_count = 0;
-}
-
-void send_status_to_master() {
-
-}
-
-int main() {
-	uart_init();
-	i2c_init_as_slave();
-	start_game_waiting();
 
 	sei();
+
+	command_reset();
 
 	for(;;);
 }
--- ex00/slave_command.c
+++ ex00/slave_command.c
@@ -1,23 +1,62 @@
 #include "slave_command.h"
 
 void command_button() {
-
+	uart_printstr("Received button command\r\n");
+	if (current_slave_state == SLAVE_STATE_STARTING) {
+		TWDR = button_pressed;
+		uart_printstr("Sending status to master: ");
+		uart_putnbr(button_pressed);
+		uart_printstr("\r\n");
+	} else if(current_slave_state == SLAVE_STATE_PLAYING) {
+		TWDR = clicked_ms; //TODO Transform to 16 bits
+		uart_printstr("Sending status to master: ");
+		uart_putnbr(clicked_ms);
+		uart_printstr("\r\n");
+	}
 }
 
 void command_start() {
-
+	uart_printstr("Received start command\r\n");
+	PORTD &= ~(1 << LED_G);
+	current_slave_state = SLAVE_STATE_PLAYING;
+	start_game_waiting();
 }
 
 void command_win() {
-
+	uart_printstr("Received win command\r\n");
+	stop_game_waiting();
+	PORTB &= ~(1 << LED1);
+	PORTB &= ~(1 << LED2);
+	PORTB &= ~(1 << LED3);
+	PORTB &= ~(1 << LED4);
+	PORTD |= (1 << LED_G);
 }
 
 void command_lose() {
-
+	uart_printstr("Received lose command\r\n");
+	stop_game_waiting();
+	PORTB &= ~(1 << LED1);
+	PORTB &= ~(1 << LED2);
+	PORTB &= ~(1 << LED3);
+	PORTB &= ~(1 << LED4);
+	PORTD |= (1 << LED_R);
 }
 
 void command_reset() {
-
+	uart_printstr("Received reset command\r\n");
+	stop_game_waiting();
+	timer_count = 0;
+	clicked_ms = 0;
+	button_pressed = 0;
+	current_slave_state = SLAVE_STATE_STARTING;
+	PORTB &= ~(1 << LED1);
+	PORTB &= ~(1 << LED2);
+	PORTB &= ~(1 << LED3);
+	PORTB &= ~(1 << LED4);
+	PORTD &= ~(1 << LED_R);
+	PORTD &= ~(1 << LED_G);
+	PORTD &= ~(1 << LED_B);
+	PORTD |= (1 << LED_R);
 }
 
 void exec_slave_command(command cmd) {
